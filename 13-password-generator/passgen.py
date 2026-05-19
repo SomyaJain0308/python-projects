3c3f35f47229c1b2d0d6436827071ae56375a4b3
Somya Jain <somyaj0308@gmail.com>
2026-05-19 16:19:07 +0000

    s

--- 13-password-generator/passgen.py
+++ 13-password-generator/passgen.py
@@ -11,17 +11,23 @@ Random Selection: random.choice() is used to pick individual characters from the
 import random, string
 while True:
     try:
-        num=int(input("How many characters do you want it to be? "))
-        if num<8:
+        num = int(input("How many characters do you want it to be? "))
+        if num < 8:
             print("Too Short let's make the password a bit discreet!")
             continue
-        elif num>16:
+        elif num > 16:
             print("Woah! are you trynna forget it? Let's make it smaller.")
             continue
         else:
             break
     except ValueError:
         print("Enter a number!")
-chars = string.ascii_letters + string.digits + string.punctuation
-chars = random.choices(chars, k=num)
-print("".join(chars))
+pool = string.ascii_letters + string.digits + string.punctuation
+while True:
+    password_list = random.choices(pool, k=num)    
+    has_letter = any(c in string.ascii_letters for c in password_list)
+    has_digit = any(c in string.digits for c in password_list)
+    has_symbol = any(c in string.punctuation for c in password_list)
+    if has_letter and has_digit and has_symbol:
+        break
+print("".join(password_list))
