6228fc8f5ee4ab3c295220ac01c1ad2c6c9af37a
Somya Jain <somyaj0308@gmail.com>
2026-05-19 12:01:18 +0530

    Create quiz.py

---
+++ 1) Quiz Game/quiz.py
@@ -0,0 +1,8 @@
+"""
+The Quiz Game is a beginner-friendly project designed to introduce fundamental programming concepts like input handling, string manipulation, 
+and basic conditional logic.
+User Interaction: The program prompts the user to play and collects their answers to a series of questions.
+Logic & Flow: It checks if the user's input matches the correct answer, typically converting the input to lowercase using .lower() to ensure case-insensitive matching.
+Score Tracking: It initializes a variable (e.g., score = 0) to keep track of correct answers, incrementing it every time the user provides a correct response.
+Feedback: After completing the questions, the program calculates the final score and provides a summary (e.g., "You got 2 questions correct") 
+"""
