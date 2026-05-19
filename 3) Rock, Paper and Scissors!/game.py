735256e5d94b795fd5f2a5009b71d83306a597c6
Somya Jain <somyaj0308@gmail.com>
2026-05-19 13:34:13 +0530

    Create game.py

---
+++ 3) Rock, Paper and Scissors!/game.py
@@ -0,0 +1,9 @@
+"""
+The third project in the video is Rock, Paper, Scissors. This project builds on previous logic by introducing the random module to simulate a computer opponent 
+and while loops for continuous gameplay.
+The Basic Idea:
+Computer Selection: The program uses random.randint to pick between 'rock', 'paper', and 'scissors'.
+Conditional Logic: It utilizes multiple elif statements to compare the user's input against the computer's choice to determine the winner.
+Score Tracking: It maintains variables (user_wins and computer_wins) to keep a running tally of the score throughout the session.
+Game Control: It implements a 'Q' input to allow the user to gracefully quit the game at any time, showing final results upon exit.
+"""
