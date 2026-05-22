c1f58ad5ba705ef915eccf751450aceca0eeb7e1
Somya Jain <somyaj0308@gmail.com>
2026-05-22 15:16:03 +0530

    Update turtle_race.py

--- 10-turtle-racing/turtle_race.py
+++ 10-turtle-racing/turtle_race.py
@@ -8,3 +8,43 @@ Object-Oriented Programming (OOP): Interacting with turtle objects (instantiatin
 Random Module: Using random.randrange() to give each racer a different movement increment, ensuring a fair but unpredictable race.
 Event Handling: Simple loops to control the game state and display the result once a winner is declared
 """
+import turtle, random
+def main():
+    screen = turtle.Screen()
+    screen.setup(width=500, height=400)
+    screen.title("The Ultimate Turtle Race")
+    user_bet = screen.textinput(
+        title="Make your bet",
+        prompt="Which turtle will win the race? Enter a color (red, orange, yellow, green, blue, purple): ",
+    )
+    if user_bet:
+        user_bet = user_bet.lower().strip()
+    else:
+        user_bet = ""
+    colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+    y_positions = [-100, -60, -20, 20, 60, 100]
+    all_turtles = []
+    for turtle_index in range(0, 6):
+        new_turtle = turtle.Turtle(shape="turtle")
+        new_turtle.color(colors[turtle_index])
+        new_turtle.penup()
+        new_turtle.goto(x=-230, y=y_positions[turtle_index])
+        all_turtles.append(new_turtle)
+    is_race_on = False
+    if user_bet:
+        is_race_on = True
+    while is_race_on:
+        for racer in all_turtles:
+            if racer.xcor() > 230:
+                is_race_on = False
+                winning_color = racer.pencolor()
+                if winning_color == user_bet:
+                    print(f"You've won! The {winning_color} turtle is the winner!")
+                else:
+                    print(f"You've lost! The {winning_color} turtle is the winner!")
+                break
+            random_distance = random.randint(0, 10)
+            racer.forward(random_distance)
+    screen.exitonclick()
+if __name__ == "__main__":
+    main()
